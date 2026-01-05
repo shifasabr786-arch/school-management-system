78d206b2a41af8d6626f60609f9b0d1d3d0547d3
Apurva Anand <152374481+apurvaanand51@users.noreply.github.com>
2026-01-05 10:09:49 +0530

    updating auth.js in routes directory

--- school-management-backend/src/routes/auth.js
+++ school-management-backend/src/routes/auth.js
@@ -1,1 +1,12 @@
+// src/routes/auth.js
+const express = require('express');
+const router = express.Router();
+const authController = require('../controllers/auth');
 
+// register
+router.post('/register', authController.register);
+
+// login
+router.post('/login', authController.login);
+
+module.exports = router;
