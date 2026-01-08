332cb0e3925d96d683f65cdf463229a7f16e38c1
Apurva Anand <152374481+apurvaanand51@users.noreply.github.com>
2026-01-08 21:28:57 +0530

    Add issue management routes with authentication

--- school-management-backend/src/routes/issues.js
+++ school-management-backend/src/routes/issues.js
@@ -1,1 +1,16 @@
+// src/routes/issues.js
+const express = require('express');
+const router = express.Router();
+const issuesController = require('../controllers/issues');
+const auth = require('../middlewares/auth');
+
+// Public listing (optionally can be protected)
+router.get('/', issuesController.listIssues);
+router.get('/:id', issuesController.getIssue);
+
+// Protected endpoints (issue & return)
+router.post('/issue', auth, issuesController.issueBook);
+router.post('/return', auth, issuesController.returnBook);
+
+module.exports = router;
 
