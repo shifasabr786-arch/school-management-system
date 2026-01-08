1a4cd9e65e448b2a73e842d1ed55fd5ad0a0949e
Apurva Anand <152374481+apurvaanand51@users.noreply.github.com>
2026-01-08 21:33:01 +0530

    Add routes for managing questions

--- school-management-backend/src/routes/questions.js
+++ school-management-backend/src/routes/questions.js
@@ -1,1 +1,17 @@
+// src/routes/questions.js
+const express = require('express');
+const router = express.Router();
+const questionsController = require('../controllers/questions');
+const auth = require('../middlewares/auth');
+
+// Public: list and get question (careful: we will not expose answers publicly)
+router.get('/', questionsController.listQuestions);
+router.get('/:id', questionsController.getQuestion);
+
+// Protected: create/update/delete questions
+router.post('/', auth, questionsController.createQuestion);
+router.put('/:id', auth, questionsController.updateQuestion);
+router.delete('/:id', auth, questionsController.deleteQuestion);
+
+module.exports = router;
 
